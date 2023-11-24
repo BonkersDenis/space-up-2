990d35ceaf3c11fa8e217be777ac9bfa8d218ee0
BonkersDenis <118104588+BonkersDenis@users.noreply.github.com>
2023-11-24 18:04:36 +0300

    space up 2

---
+++ Assets/Scripts/BossInfo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Match3
+{
+    public class BossInfo : MonoBehaviour
+    {
+        public int Life;
+    }
+}
